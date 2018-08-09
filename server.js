2d2954e0e6b7bf30a0ce5a7210359e8a6175b43e
Matt Eubanks <spc.eubanks.matthew@gmail.com>
2018-08-08 19:47:05 -0700

    Start Routes

--- server.js
+++ server.js
@@ -9,9 +9,27 @@ mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser
 
 app.use(express.static('public'));
 
+
 app.get("/", function (req, res) {
     res.send("Here I am");
 })
+
+//MAIN PAGE SHOWS MOST RECENT POSTS
+//INDEX ROUTE
+app.get("/blogs", function (req, res) {
+
+});
+//HOME PAGE SHOWS USER POSTS 
+
+//LOGIN PAGE
+
+//SIGN UP PAGE
+
+//NEW POST PAGE
+
+//EDIT POST PAGE
+
+//COMMENTS PAGE
 
 if (require.main === module) {
     app.listen(process.env.PORT || 8080, function () {
