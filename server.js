51b26583efa62abb5f5b55e5dbe2aa14936fc6dc
Matt Eubanks <spc.eubanks.matthew@gmail.com>
2018-08-08 17:58:18 -0700

    new

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const express = require('express');
-const app = express();
+var mongoose = require("mongoose"),
+    express = require('express'),
+    morgan = require("morgan"),
+    app = express();
+
+
+//APP CONFIG
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true })
 
 app.use(express.static('public'));
 
-if(require.main === module) {
-    app.listen(process.env.PORT || 8080, function() {
+app.get("/", function (req, res) {
+    res.send("Here I am");
+})
+
+if (require.main === module) {
+    app.listen(process.env.PORT || 8080, function () {
         console.info(`App listening on ${this.address().port}`);
     });
 }
--- test/test-server.js
+++ test/test-server.js
@@ -6,12 +6,12 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe("index page", function() {
-    it("should exist", function() {
+describe("index page", function () {
+    it("should exist", function () {
         return chai
             .request(app)
             .get("/")
-            .then(function(res) {
+            .then(function (res) {
                 expect(res).to.have.status(200);
             });
     });
